eaea63f2ead15c3a0285006a104bc21d364e9666
Aleksey Belchenkov <u608110@gmail.com>
2020-04-16 17:21:10 +0300

    Buid adaptime home page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,8 +10,8 @@
 </head>
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
-        <div class="container px-4 mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center">
+        <div class="container px-4 mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex items-center flex-col md:flex-row">
                 <li>
                     <a href="#" class="flex items-center">
                         <div>
@@ -21,18 +21,18 @@
                         <div class="logo-text">Movie App</div>
                     </a>
                 </li>
-                <li class="ml-16">
+                <li class="md:ml-16 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Movies</a>
                 </li>
-                <li class="ml-6">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">TV Shows</a>
                 </li>
-                <li class="ml-6">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Actors</a>
                 </li>
             </ul>
-            <div class="flex items-center">
-                <div class="relative">
+            <div class="flex flex-col md:flex-row items-center">
+                <div class="relative mt-3 md:mt-0">
                     <input
                         type="text"
                         name=""
@@ -44,7 +44,7 @@
                         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
                     </div>
                 </div>
-                <div class="ml-4">
+                <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
                         <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8" />
                     </a>
