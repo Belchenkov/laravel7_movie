efd35b502f939004f53fda40da6fd7607d8a165c
Aleksey Belchenkov <u608110@gmail.com>
2020-05-21 16:40:11 +0300

    Apply ViewModel for output nowPlayingMovies

--- app/ViewModels/MoviesViewModel.php
+++ app/ViewModels/MoviesViewModel.php
@@ -20,22 +20,27 @@ class MoviesViewModel extends ViewModel
 
     public function popularMovies()
     {
-        return collect($this->popularMovies)->map(static function ($movie) {
+        return $this->formatMovies($this->popularMovies);
+    }
+
+    public function nowPlayingMovies()
+    {
+        return $this->formatMovies($this->nowPlayingMovies);
+    }
+
+    public function genres()
+    {
+        return $this->genres;
+    }
+
+    public function formatMovies($movies)
+    {
+        return collect($movies)->map(static function ($movie) {
             return collect($movie)->merge([
                 'poster_path' => 'https://image.tmdb.org/t/p/w500' . $movie['poster_path'],
                 'vote_average' => $movie['vote_average'] * 10 . '%',
                 'release_date' =>  \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y')
             ]);
         });
-    }
-
-    public function nowPlayingMovies()
-    {
-        return $this->nowPlayingMovies;
-    }
-
-    public function genres()
-    {
-        return $this->genres;
     }
 }
