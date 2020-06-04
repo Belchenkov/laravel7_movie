02ae3f2fd1ed8d666ca6a64229472eb4da3612b8
Aleksey Belchenkov <u608110@gmail.com>
2020-06-04 17:04:45 +0300

    Output actors avatars

--- app/ViewModels/ActorsViewModel.php
+++ app/ViewModels/ActorsViewModel.php
@@ -15,6 +15,16 @@ class ActorsViewModel extends ViewModel
 
     public function popularActors()
     {
-        return $this->popularActors;
+        return collect($this->popularActors)->map(function ($actor) {
+            return collect($actor)->merge([
+                'profile_path' => $actor['profile_path']
+                    ? 'https://image.tmdb.org/t/p/w235_and_h235_face' . $actor['profile_path']
+                    : 'https://ui-avatars.com/api/?size=235&name=' . $actor['name'],
+            ])->only([
+                'profile_path',
+                'id',
+                'name'
+            ]);
+        });
     }
 }
--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container mx-auto px-4 pt-16">
+    <div class="container mx-auto px-4 py-16">
         <div class="popular-actors">
             <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
                 Popular Actors
             </h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                <div class="actor mt-8">
-                    <a href="#">
-                        <img
-                            src=""
-                            alt="profile image"
-                            class="hover:opacity-75 transition ease-in-out duration-150"
-                        >
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg hover:text-gray-300">Actor Name</a>
-                        <div class="text-sm truncate text-gray-400">Iron Man</div>
+                @foreach($popularActors as $actor)
+                    <div class="actor mt-8">
+                        <a href="#">
+                            <img
+                                src="{{ $actor['profile_path'] }}"
+                                alt="profile image"
+                                class="hover:opacity-75 transition ease-in-out duration-150"
+                            >
+                        </a>
+                        <div class="mt-2">
+                            <a href="#" class="text-lg hover:text-gray-300">{{ $actor['name'] }}</a>
+                            <div class="text-sm truncate text-gray-400">Iron Man</div>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div> <!-- end popular-actors -->
 @endsection
