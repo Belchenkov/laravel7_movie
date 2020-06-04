f665fda2127bc7ac42e11dfba9f7500ad4413b34
Aleksey Belchenkov <u608110@gmail.com>
2020-06-04 17:13:03 +0300

    Display actors info

--- app/ViewModels/ActorsViewModel.php
+++ app/ViewModels/ActorsViewModel.php
@@ -20,10 +20,20 @@ class ActorsViewModel extends ViewModel
                 'profile_path' => $actor['profile_path']
                     ? 'https://image.tmdb.org/t/p/w235_and_h235_face' . $actor['profile_path']
                     : 'https://ui-avatars.com/api/?size=235&name=' . $actor['name'],
+                'known_for' => collect($actor['known_for'])
+                    ->where('media_type', 'movie')
+                    ->pluck('title')
+                    ->union(
+                        collect($actor['known_for'])
+                            ->where('media_type', 'tv')
+                            ->pluck('name')
+                    )
+                    ->implode(', ')
             ])->only([
                 'profile_path',
                 'id',
-                'name'
+                'name',
+                'known_for'
             ]);
         });
     }
